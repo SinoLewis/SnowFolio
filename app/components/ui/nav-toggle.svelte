b1207cbb89e9618686d5dbb0a8dec074a367d3d7
SinoLewis <sinolewis@gmail.com>
2023-03-21 03:16:41 +0300

    Init svelte dev server

---
+++ app/components/ui/nav-toggle.svelte
@@ -0,0 +1,12 @@
+<svelte:options tag="nav-toggle" />
+
+<script lang="ts">
+</script>
+
+<modal-dialog name="nav" esc="true">
+  <h1>NavLinks</h1>
+  <p>seriously:</p>
+</modal-dialog>
+
+<style>
+</style>
